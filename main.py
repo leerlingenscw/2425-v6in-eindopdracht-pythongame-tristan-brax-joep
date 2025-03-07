554b2ad82e98529c8657914669acf6995a869c7f
Tristan121314 <131338@stanislascollege.net>
2025-03-07 12:10:07 +0000

    bg

--- main.py
+++ main.py
@@ -8,11 +8,21 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Braxbuistin')
 
+
+bg_image = pygame.image.load('Assets/PygameBG.webp').convert_alpha()
+
 run = True
 while run:
+
+    screen.blit(bg_image, (0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    pygame.display.update()
+    
+
+
 
 pygame.quit()
 

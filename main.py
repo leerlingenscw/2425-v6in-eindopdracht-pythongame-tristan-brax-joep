9a97a999c1fc35bf9f94560e535b3597393166d0
Tristan121314 <131338@stanislascollege.net>
2025-03-12 13:12:42 +0000

    yur

--- main.py
+++ main.py
@@ -8,17 +8,50 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Braxbuistin')
 
+clock = pygame.time.Clock()
+FPS = 60
+
+
+WHITE = (255, 255, 255)
+
+
 jumpy_image = pygame.image.load('Assets/pixil-frame-0.png').convert_alpha()
 bg_image = pygame.image.load('Assets/PygameBG.webp').convert_alpha()
 
 class Player():
     def __init__(self, x, y):
-        self.image = pygame.transform.scale(jumpy_image, (150, 150))
-        self.rect = self.image.get_rect()
+        self.image = pygame.transform.scale(jumpy_image, (45, 45))
+        self.width = 25
+        self.height = 40
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = (x, y)
+        self.flip = False
+    
+    def move(self):
+        dx = 0
+        dy = 0
+
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_a]:
+            dx = -10
+            self.flip = True
+        if key[pygame.K_d]:
+            dx = 10
+            self.flip = False
+        
+        if self.rect.left + dx < 0:
+            dx = 0 - self.rect.left
+        if self.rect.right + dx > SCREEN_WIDTH:
+            dx = SCREEN_WIDTH - self.rect.right
+
+          
+        self.rect.x += dx
+        self.rect.y += dy
 
     def draw(self):
-        screen.blit(self.image, self.rect)
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), (self.rect.x -12, self.rect.y -5))
+        pygame.draw.rect(screen, WHITE, self.rect, 2)
 
 
 
@@ -28,6 +61,10 @@ jumpy = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
 
 run = True
 while run:
+
+    clock.tick(FPS)
+    
+    jumpy.move()
 
     screen.blit(bg_image, (0, 0))
 
